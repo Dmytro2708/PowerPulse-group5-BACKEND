dcf4e98cb9375a8a86adf1315cf1ab73f7889fee
YevheniiaHr <131554616+YevheniiaHr@users.noreply.github.com>
2024-03-04 19:23:00 +0100

    add error handling

--- controllers/auth/products.js
+++ controllers/auth/products.js
@@ -1,29 +1,33 @@
 const fs = require("fs").promises;
 const path = require("path");
 
-const { ctrlWrapper } = require("../../helpers");
+const { ctrlWrapper, HttpError } = require("../../helpers");
 const Product = require("../../models/products");
 
 const productsPath = path.resolve("productsCategories.json");
 
 const getProductsCategories = async () => {
-  const products = await fs.readFile(productsPath);
-
+  const products = await fs.readFile(productsPath, "utf8");
+  if (!products) {
+    throw HttpError(404, "Not found");
+  }
   return JSON.parse(products);
 };
-// getProductsCategories()
-//   .then((products) => console.log(products))
-//   .catch((error) => console.error(error));
-// console.log("Request received to getProductsCategories");
+
 const getProductsByBloodType = async (req, res) => {
-  const userBloodType = req.user.bloodType;
+  const userBloodType = req.user.blood;
+
   const allowedProducts = await Product.find({
-    groupBloodNotAllowed: { [userBloodType]: true },
+    [`groupBloodNotAllowed.${userBloodType}`]: false,
   });
   const disallowedProducts = await Product.find({
-    groupBloodNotAllowed: { [userBloodType]: false },
+    [`groupBloodNotAllowed.${userBloodType}`]: true,
   });
-  res.json({ allowedProducts, disallowedProducts });
+  if (!allowedProducts || !disallowedProducts) {
+    throw HttpError(404, "Not found");
+  }
+
+  res.status(200).json({ allowedProducts, disallowedProducts });
 };
 
 module.exports = {
