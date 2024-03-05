dd66f9c3602891d981891fdc10335fdc46f5c630
Dmytro2708 <130864203+Dmytro2708@users.noreply.github.com>
2024-03-05 21:48:31 +0200

    add swagger.json  fix auth controllers

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -7,30 +7,31 @@ const { User } = require("../../models/user");
 
 const { HttpError, ctrlWrapper } = require("../../helpers");
 
-
 const login = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
 
   if (!user) {
-    throw HttpError(401, 'Email or password is wrong');
+    throw HttpError(401, "Email or password is wrong");
   }
 
   const isPasswordValid = await bcrypt.compare(password, user.password);
 
   if (!isPasswordValid) {
-    throw HttpError(401, 'Email or password is wrong');
+    throw HttpError(401, "Email or password is wrong");
   }
 
   const payload = { id: user._id };
-  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '23h' });
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
 
   await User.findByIdAndUpdate(user._id, { token });
 
   res.status(200).json({
+    user: {
+      name: user.name,
+      email: user.email },
     token: token,
   });
 };
 
-
-  module.exports = ctrlWrapper(login);
+module.exports = ctrlWrapper(login);
