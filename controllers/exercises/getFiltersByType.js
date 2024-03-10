35cba6a379862e80f2aeac5a3574691ff46c0c0e
YevheniiaHr <131554616+YevheniiaHr@users.noreply.github.com>
2024-03-10 19:12:39 +0100

    fix getFiltersByType

--- controllers/exercises/getFiltersByType.js
+++ controllers/exercises/getFiltersByType.js
@@ -2,9 +2,8 @@ const { HttpError, ctrlWrapper } = require("../../helpers");
 const Filter = require("../../models/filters");
 
 const getFiltersByType = async (req, res) => {
-  const filterType = req.query.type;
-  const filters = await Filter.find({ filter: filterType });
-  if (!filters) {
+  const filters = await Filter.find({});
+  if (!filters || filters.length === 0) {
     throw HttpError(404, "Filters not found");
   }
   res.status(200).json(filters);
